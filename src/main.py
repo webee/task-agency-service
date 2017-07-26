f0d042f0d9bd4bcf99437e3c25c83fd77b014f34
wang.yi <wang.yi@qinqinxiaobao.com>
2017-07-26 18:11:28 +0800

    update.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,36 @@
 from gevent import monkey
-from services.test import TestTasksManager
-
 monkey.patch_all()
 
-test_tasks_manager = TestTasksManager()
 
-methods = {
-    'test_fetch_start': test_tasks_manager.start,
-    'test_fetch_resume': test_tasks_manager.resume,
-}
+def get_test_methods() -> dict:
+    from services.service import SessionTasksManager
+    from services.service import UUIDSessionIDGenerator
+    from services.service import MemorySessionStorage
+    from services.service import PathTaskClassFinder
+
+    uuid_session_id_generator = UUIDSessionIDGenerator()
+    memory_session_storage = MemorySessionStorage()
+    test_task_finder = PathTaskClassFinder(lambda task_id: 'services.test.tasks.t_%s' % task_id, 'Task')
+    test_tasks_manager = SessionTasksManager(uuid_session_id_generator,
+                                             memory_session_storage,
+                                             test_task_finder)
+    return {
+        'fetch_start': test_tasks_manager.start,
+        'fetch_resume': test_tasks_manager.resume,
+    }
+
+
+def get_methods():
+    methods = {}
+
+    for name, method in get_test_methods().items():
+        methods['test_' + name] = method
+
+    return methods
+
 
 if __name__ == '__main__':
     import zerorpc
-    server = zerorpc.Server(methods, name='task-agency-service', heartbeat=30)
+    server = zerorpc.Server(get_methods(), name='task-agency-service', heartbeat=30)
     server.bind('tcp://*:12345')
     server.run()
--- src/services/service.py
+++ src/services/service.py
@@ -1,3 +1,5 @@
+import importlib
+from typing import List, Callable
 from abc import ABCMeta, abstractmethod
 import uuid
 
@@ -37,6 +39,21 @@ class AbsSessionTask(AbsTask):
 
     @abstractmethod
     def run(self, params: dict):
+        try:
+            res = self._do_run(params)
+            self._set_done()
+            # TODO: 正常返回
+        except AskForParamsError as e:
+            # TODO: 请求参数
+            pass
+        except Exception as e:
+            self._set_done()
+            # TODO: 错误返回
+            return {
+            }
+
+    @abstractmethod
+    def _do_run(self, params: dict):
         raise NotImplementedError()
 
     @property
@@ -51,14 +68,54 @@ class AbsSessionTask(AbsTask):
         self._done = True
 
 
-class AbsSessionTasksManager(metaclass=ABCMeta):
+class AbsSessionStorage(metaclass=ABCMeta):
+    @abstractmethod
+    def get_session(self, session_id: str) -> SessionData:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def remove_session(self, session_id: str):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def save_session(self, session_data: SessionData):
+        raise NotImplementedError()
+
+
+class AbsSessionIDGenerator(metaclass=ABCMeta):
+    @abstractmethod
+    def new(self, *args, **kwargs) -> str:
+        raise NotImplementedError()
+
+
+class AbsTaskClassFinder(metaclass=ABCMeta):
+    @abstractmethod
+    def find(self, task_id: str):
+        raise NotImplementedError()
+
+
+class AbsResultHandler(metaclass=ABCMeta):
+    @abstractmethod
+    def handle(self, result: dict):
+        raise NotImplementedError()
+
+
+class SessionTasksManager(object):
+    def __init__(self, session_id_generator: AbsSessionIDGenerator,
+                 session_storage: AbsSessionStorage, task_class_finder: AbsTaskClassFinder):
+        super().__init__()
+        self._sig = session_id_generator
+        self._ss = session_storage
+        self._tcf = task_class_finder
+        self._result_handlers: List[AbsResultHandler] = []
+
     def start(self, task_id):
         """
         start a task by task_id
         :param task_id: task type id
         :return: session_id, task result
         """
-        session_id = self._new_session_id()
+        session_id = self._sig.new()
         session_data = SessionData(session_id, task_id, {}, {})
         return session_id, self._run(session_data, {})
 
@@ -69,59 +126,57 @@ class AbsSessionTasksManager(metaclass=ABCMeta):
         :param params: params
         :return: session_id, task result
         """
-        session_data = self._get_session_data(session_id)
+        session_data = self._ss.get_session(session_id)
         if session_data is None:
             raise PermissionError('session not exists')
 
         return session_id, self._run(session_data, params)
 
+    def register_result_handler(self, handler: AbsResultHandler):
+        self._result_handlers.append(handler)
+
     def _run(self, session_data: SessionData, params: dict):
         task = self._get_task(session_data)
         res = task.run(params)
         if task.done:
-            self._remove_session(session_data.id)
+            result = task.session_data.result
+            for handler in self._result_handlers:
+                handler.handle(result)
+            self._ss.remove_session(session_data.id)
         else:
-            self._save_session(task.session_data)
+            self._ss.save_session(task.session_data)
         return res
 
     def _get_task(self, session_data: SessionData) -> AbsSessionTask:
         task_id = session_data.task_id
-        task_cls = self._get_task_cls(task_id)
+        task_cls = self._tcf.find(task_id)
         return task_cls(session_data)
 
-    @abstractmethod
-    def _get_task_cls(self, task_id: str):
-        raise NotImplementedError()
 
-    @abstractmethod
-    def _new_session_id(self) -> str:
-        raise NotImplementedError()
-
-    @abstractmethod
-    def _get_session(self, session_id: str) -> SessionData:
-        raise NotImplementedError()
-
-    @abstractmethod
-    def _remove_session(self, session_id: str):
-        raise NotImplementedError()
-
-    @abstractmethod
-    def _save_session(self, session_data: SessionData):
-        raise NotImplementedError()
-
-
-class TestAbsMemorySessionTasksManager(AbsSessionTasksManager):
-    def _new_session_id(self) -> str:
+class UUIDSessionIDGenerator(AbsSessionIDGenerator):
+    def new(self) -> str:
         return uuid.uuid4().hex
 
+
+class MemorySessionStorage(AbsSessionStorage):
     def __init__(self):
         self.__sessions = {}
 
-    def _save_session(self, session_data: SessionData):
+    def save_session(self, session_data: SessionData):
         self.__sessions[session_data.id] = session_data
 
-    def _get_session(self, session_id: str) -> SessionData:
+    def get_session(self, session_id: str) -> SessionData:
         return self.__sessions.get(session_id)
 
-    def _remove_session(self, session_id: str):
+    def remove_session(self, session_id: str):
         del self.__sessions[session_id]
+
+
+class PathTaskClassFinder(AbsTaskClassFinder):
+    def __init__(self, get_path_func: Callable, cls_name: str):
+        self._get_path_func = get_path_func
+        self._cls_name = cls_name
+
+    def find(self, task_id: str):
+        m = importlib.import_module(self._get_path_func(task_id))
+        return getattr(m, self._cls_name)
