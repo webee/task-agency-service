61cd6d209e5c66c140bf6a4c11a998d294883b66
feifei.cheng <cff@qinqinxiaobao.com>
2017-08-31 16:03:45 +0800

    update

--- src/services/social_insurance/tasks/c_120100/__init__.py
+++ src/services/social_insurance/tasks/c_120100/__init__.py
@@ -28,14 +28,11 @@ class Task(AbsTaskUnitSessionTask):
 
         # result
         result: dict = self.result
-        result.setdefault('key',{})
         result.setdefault('meta', {})
         result.setdefault('data', {})
-        result.setdefault('detailEI',{})       # 养老
-        result.setdefault('detailHI', {})      # 医疗
-        result.setdefault('detailCI', {})      # 工伤
-        result.setdefault('detailII', {})      # 失业
-        result.setdefault('detailBI', {})      # 生育
+        self.result['data']['baseInfo']={}
+        result.setdefault('key', {})
+
 
     def _update_session_data(self):
         super()._update_session_data()
@@ -53,7 +50,7 @@ class Task(AbsTaskUnitSessionTask):
     # noinspection PyMethodMayBeStatic
     def _check_login_params(self, params):
         assert params is not None, '缺少参数'
-        assert 'id_num' in params, '缺少身份证号'
+        assert 'id_num' in params, '缺少用户名'
         assert 'account_pass' in params, '缺少密码'
         assert 'vc' in params, '缺少验证码'
         # other check
@@ -66,6 +63,17 @@ class Task(AbsTaskUnitSessionTask):
         CaptchaIds=soup.text[7:].replace('"}','')
         resp = self.s.get(vc_url)
         return dict(content=resp.content, content_type=resp.headers['Content-Type'])
+
+    #转换五险状态
+    def _convert_type(self,nums):
+        res=""
+        if(nums=="1"):
+            res="参保"
+        elif(nums=="2"):
+            res="暂停"
+        elif(nums=="3"):
+            res="终止"
+        return res
 
     def _unit_login(self, params=None):
         err_msg = None
@@ -88,7 +96,7 @@ class Task(AbsTaskUnitSessionTask):
 
                 self.result['key'] = id_num
                 self.result['meta'] = {
-                    '身份证号': id_num,
+                    '用户名': id_num,
                     '登录密码': account_pass
                 }
 
@@ -111,108 +119,205 @@ class Task(AbsTaskUnitSessionTask):
 
             respon = self.s.get("http://public.tj.hrss.gov.cn/ehrss-si-person/api/rights/persons/" + str(s))
             rs = json.loads((respon.text))
-            if rs['sex']=="1":            #性别
-                sexs='男'
-            elif rs['sex']=="2":
-                sexs='女'
+            # if rs['sex']=="1":            #性别
+            #     sexs='男'
+            # elif rs['sex']=="2":
+            #     sexs='女'
+            # if rs['mobileNumber']==None:     #联系电话
+            #     mobileNumber=""
+            # else:
+            #     mobileNumber=rs['mobileNumber']
+            # rsep = self.s.get("http://public.tj.hrss.gov.cn/ehrss-si-person/api/selectcodes")
+            # res = json.loads(rsep.text)["codeList"]
+            # for nt in range(len(res["NATION"])):                                                      #民族
+            #     if res["NATION"][nt]["value"] == rs['nation']:
+            #         nations=res["NATION"][nt]["name"]
+            #         break
+            # for ht in range(len(res["HOUSEHOLD_TYPE"])):                                              #户口性质
+            #     if res["HOUSEHOLD_TYPE"][ht]["value"] == rs['householdType']:
+            #         householdTypes=res["HOUSEHOLD_TYPE"][ht]["name"]
+            #         break
 
-            if rs['mobileNumber']==None:     #联系电话
-                mobileNumber=""
-            else:
-                mobileNumber=rs['mobileNumber']
-
-            rsep = self.s.get("http://public.tj.hrss.gov.cn/ehrss-si-person/api/selectcodes")
-            res = json.loads(rsep.text)["codeList"]
-            for nt in range(len(res["NATION"])):                                                      #民族
-                if res["NATION"][nt]["value"] == rs['nation']:
-                    nations=res["NATION"][nt]["name"]
-                    break
-
-            for ht in range(len(res["HOUSEHOLD_TYPE"])):                                              #户口性质
-                if res["HOUSEHOLD_TYPE"][ht]["value"] == rs['householdType']:
-                    householdTypes=res["HOUSEHOLD_TYPE"][ht]["name"]
-                    break
-
-            self.result['data']['userInfo'] = {
-                '姓名': rs['name'],
-                '性别': sexs,
-                '民族':nations,
-                '出生日期':rs['birthday'],
-                '证件号码':rs['idNumber'],
-                '社保卡号':rs['socialSecurityCardNumber'],
-                '手机号码':mobileNumber,
-                '参加工作日期':rs['workDate'],
-                '户口性质':householdTypes,
-                #'户口所在地详址':rs['householdAddress']
-            }
 
             #社保明细-----养老
-            beginTime=input("请输入需要查询的开始年月：")
-            endTime=input("请输入需要查询的结束年月：")
+
+            #社保缴费明细
+            self.result['data']["old_age"] = {                                  # 养老
+                "data": {}
+            }
+            basedataE = self.result['data']["old_age"]["data"]
+            modelE = {}
+
+            self.result['data']["medical_care"] = {                            # 医疗
+                "data": {}
+            }
+            basedataH = self.result['data']["medical_care"]["data"]
+            modelH = {}
+
+            self.result['data']["unemployment"] = {                             # 失业
+                "data": {}
+            }
+            basedataI = self.result['data']["unemployment"]["data"]
+            modelI = {}
+
+            self.result['data']["injuries"] = {                             # 工伤
+                "data": {}
+            }
+            basedataC = self.result['data']["injuries"]["data"]
+            modelC = {}
+
+            self.result['data']["maternity"] = {                             # 生育
+                "data": {}
+            }
+            basedataB = self.result['data']["maternity"]["data"]
+            modelB = {}
+
+            perTotalold=0.00;
+            perTotalMedical=0.00;
+            beginTime=rs['workDate'][0:6]   #查询开始时间
+            endTime=time.strftime("%Y%m",time.localtime())     #查询结束时间
             paymentFlag="1"
+
+            # 社保明细-----养老
             rpEI=self.s.get(Detail_URL+str(s) + "?beginTime=" + beginTime + "&endTime=" + endTime + "&insureCode=110&paymentFlag="+paymentFlag)
             detailEI=json.loads(rpEI.text)['empPaymentDTO']
             for a in range(len(detailEI)):
-                self.result['detailEI'][str(detailEI[a]['payDate'])]={
-                    '缴费单位':detailEI[a]['payCompany'],
-                    '缴费年月':detailEI[a]['payDate'],
-                    '缴费基数':detailEI[a]['payBase'],
-                    '单位缴存额':detailEI[a]['companyOverallPay'],
-                    '个人缴存额':detailEI[a]['personPay'],
-                    '缴费合计':detailEI[a]['payCount']
+                years=str(detailEI[a]['payDate'])[0:4]
+                months=str(detailEI[a]['payDate'])[4:6]
+                basedataE.setdefault(years,{})
+                basedataE[years].setdefault(months,[])
+
+                modelE={
+                    '缴费单位': detailEI[a]['payCompany'],
+                    '缴费年月': detailEI[a]['payDate'],
+                    '缴费基数': detailEI[a]['payBase'],
+                    '单位缴存额': detailEI[a]['companyOverallPay'],
+                    '个人缴存额': detailEI[a]['personPay'],
                 }
+
+                perTotalold +=float(detailEI[a]['personPay'])   #个人养老累积缴费
+                basedataE[years][months].append(modelE)
+
 
             # 社保明细-----医疗
             rpHI = self.s.get(Detail_URL + str(s) + "?beginTime=" + beginTime + "&endTime=" + endTime + "&insureCode=310&paymentFlag=" + paymentFlag)
             detailHI = json.loads(rpHI.text)['empPaymentDTO']
             for b in range(len(detailHI)):
-                self.result['detailHI'][str(detailHI[b]['payDate'])] = {
+                yearH = str(detailHI[b]['payDate'])[0:4]
+                monthH = str(detailHI[b]['payDate'])[4:6]
+                basedataH.setdefault(yearH, {})
+                basedataH[yearH].setdefault(monthH, [])
+
+                modelH={
                     '缴费单位': detailHI[b]['payCompany'],
                     '缴费年月': detailHI[b]['payDate'],
                     '缴费基数': detailHI[b]['payBase'],
                     '单位缴存额': detailHI[b]['companyOverallPay'],
                     '个人缴存额': detailHI[b]['personPay'],
-                    '缴费合计': detailHI[b]['payCount']
+                    # '缴费合计': detailHI[b]['payCount']
                 }
+
+                perTotalMedical +=float(detailHI[b]['personPay'])    #个人医疗累积缴费
+                basedataH[yearH][monthH].append(modelH)
+
 
             # 社保明细-----工伤
             rpCI = self.s.get(Detail_URL + str(s) + "?beginTime=" + beginTime + "&endTime=" + endTime + "&insureCode=410&paymentFlag=" + paymentFlag)
             detailCI = json.loads(rpCI.text)['empPaymentDTO']
             for c in range(len(detailCI)):
-                self.result['detailCI'][str(detailCI[c]['payDate'])] = {
+                yearC = str(detailCI[c]['payDate'])[0:4]
+                monthC = str(detailCI[c]['payDate'])[4:6]
+                basedataC.setdefault(yearC, {})
+                basedataC[yearC].setdefault(monthC, [])
+
+                modelC={
                     '缴费单位': detailCI[c]['payCompany'],
                     '缴费年月': detailCI[c]['payDate'],
                     '缴费基数': detailCI[c]['payBase'],
                     '单位缴存额': detailCI[c]['companyOverallPay'],
                     '个人缴存额': detailCI[c]['personPay'],
-                    '缴费合计': detailCI[c]['payCount']
                 }
+
+                basedataC[yearC][monthC].append(modelC)
+
 
             # 社保明细-----失业
             rpII = self.s.get(Detail_URL + str(s) + "?beginTime=" + beginTime + "&endTime=" + endTime + "&insureCode=210&paymentFlag=" + paymentFlag)
             detailII = json.loads(rpII.text)['empPaymentDTO']
             for d in range(len(detailII)):
-                self.result['detailII'][str(detailII[d]['payDate'])] = {
+                yearI = str(detailII[d]['payDate'])[0:4]
+                monthI = str(detailII[d]['payDate'])[4:6]
+                basedataI.setdefault(yearI, {})
+                basedataI[yearI].setdefault(monthI, [])
+
+                modelI={
                     '缴费单位': detailII[d]['payCompany'],
                     '缴费年月': detailII[d]['payDate'],
                     '缴费基数': detailII[d]['payBase'],
                     '单位缴存额': detailII[d]['companyOverallPay'],
                     '个人缴存额': detailII[d]['personPay'],
-                    '缴费合计': detailII[d]['payCount']
                 }
+
+                basedataI[yearI][monthI].append(modelI)
+
 
             # 社保明细-----生育
             rpBI = self.s.get(Detail_URL + str(s) + "?beginTime=" + beginTime + "&endTime=" + endTime + "&insureCode=510&paymentFlag=" + paymentFlag)
             detailBI = json.loads(rpBI.text)['empPaymentDTO']
             for f in range(len(detailBI)):
-                self.result['detailBI'][str(detailBI[f]['payDate'])] = {
+                yearB = str(detailBI[f]['payDate'])[0:4]
+                monthB = str(detailBI[f]['payDate'])[4:6]
+                basedataB.setdefault(yearB, {})
+                basedataB[yearB].setdefault(monthB, [])
+
+                modelB={
                     '缴费单位': detailBI[f]['payCompany'],
                     '缴费年月': detailBI[f]['payDate'],
                     '缴费基数': detailBI[f]['payBase'],
                     '单位缴存额': detailBI[f]['companyOverallPay'],
                     '个人缴存额': detailBI[f]['personPay'],
-                    '缴费合计': detailBI[f]['payCount']
                 }
+
+                basedataB[yearB][monthB].append(modelB)
+
+
+            #五险状态
+            stype=json.loads(self.s.get("http://public.tj.hrss.gov.cn/ehrss-si-person/api/rights/insure/"+ str(s) +"").text)
+            social_Type={
+                '养老':self._convert_type(stype[0]['paymentState']),
+                '医疗':self._convert_type(stype[2]['paymentState']),
+                '失业':self._convert_type(stype[1]['paymentState']),
+                '工伤':self._convert_type(stype[5]['paymentState']),
+                '生育':self._convert_type(stype[6]['paymentState'])
+            }
+
+            #个人基本详细信息
+            counts=0;
+            if(len(detailEI)<=len(detailHI)):
+                counts=len(detailHI)
+            else:
+                counts=len(detailEI)
+
+            self.result['data']['baseInfo'] = {
+                '姓名': rs['name'],
+                '身份证号': rs['idNumber'],
+                '更新时间': time.strftime("%Y-%m-%d", time.localtime()),
+                '城市名称': '天津市',
+                '城市编号': '120100',
+                '缴费时长': counts,
+                '最近缴费时间': detailEI[len(detailEI)-1]['payDate'],
+                '开始缴费时间': rs['workDate'][0:6],
+                '个人养老累积缴费': perTotalold,
+                '个人医疗累积缴费': perTotalMedical,
+                '五险状态':social_Type
+                # '性别': sexs,
+                # '民族':nations,
+                # '出生日期':rs['birthday'],
+                # '社保卡号':rs['socialSecurityCardNumber'],
+                # '手机号码':mobileNumber,
+                # '户口性质':householdTypes,
+                # '户口所在地详址':rs['householdAddress']
+            }
 
             return
         except PermissionError as e:
