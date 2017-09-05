4feee6a0ef72e4b5c87cfc2a16341fabbbc6d6c8
wang.yi <wang.yi@qinqinxiaobao.com>
2017-09-05 10:32:45 +0800

    fix.

--- src/services/social_insurance/tasks/c_110100/__init__.py
+++ src/services/social_insurance/tasks/c_110100/__init__.py
@@ -638,7 +638,7 @@ class Task(AbsTaskUnitSessionTask):
     # 验证码
     def _new_vc(self):
         resp = self.s.get(VC_URL)
-        return dict(cls='data:image', content=resp.content, content_type=resp.headers['Content-Type'])
+        return dict(cls='data:image', content=resp.content, content_type=resp.headers.get('Content-Type'))
 
 
 if __name__ == '__main__':
