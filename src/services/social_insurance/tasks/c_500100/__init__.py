ac53fbd6fcdcf153bce4a1e4d9e3459b17996835
wang.yi <wang.yi@qinqinxiaobao.com>
2017-09-05 11:22:27 +0800

    fix vc.

--- src/services/social_insurance/tasks/c_500100/__init__.py
+++ src/services/social_insurance/tasks/c_500100/__init__.py
@@ -563,7 +563,7 @@ class Task(AbsFetchTask):
     # 刷新验证码
     def _new_vc(self):
         resp = self.s.get(VC_URL)
-        return dict(cls='data:image', content=resp.content, content_type=resp.headers['Content-Type'])
+        return dict(cls='data:image', content=resp.content, content_type=resp.headers.get('Content-Type'))
 
 
 if __name__ == '__main__':
