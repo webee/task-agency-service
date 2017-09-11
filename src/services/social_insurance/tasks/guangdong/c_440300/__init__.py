780651f9966ed1a1485fd2cc0fa832da6824610a
boyuanyuan <byy@qinqinxiaobao.com>
2017-09-11 15:58:35 +0800

    Merge remote-tracking branch 'origin/master' into boyuanyuan
    # Conflicts:
    #	src/services/social_insurance/tasks/guangdong/c_440300/__init__.py

--- src/services/social_insurance/tasks/guangdong/c_440300/__init__.py
+++ src/services/social_insurance/tasks/guangdong/c_440300/__init__.py
@@ -3,12 +3,28 @@ import time
 import random
 import json
 import datetime
+from services.webdriver import new_driver, DriverRequestsCoordinator
 from bs4 import BeautifulSoup
 from services.service import SessionData
 from services.service import AskForParamsError, PreconditionNotSatisfiedError, TaskNotAvailableError
 from services.errors import InvalidParamsError, TaskNotImplementedError
 from services.commons import AbsFetchTask
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+class value_is_number(object):
+    """判断元素value是数字"""
+    def __init__(self, locator):
+        self.locator = locator
+
+    def __call__(self, driver):
+        element = driver.find_element(*self.locator)
+        val = element.get_attribute('value')
+        return val and val.isnumeric()
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0"
+LOGIN_PAGE_URL='https://seyb.szsi.gov.cn/web/ggfw/app/ggfwpf/login/yx/views/public/login/login_comm.html'
 LOGIN_URL = 'https://seyb.szsi.gov.cn/web/ajaxlogin.do'
 VC_URL = 'https://seyb.szsi.gov.cn/web/ImageCheck.jpg'
 USERINFO_URL='https://seyb.szsi.gov.cn/web/ajax.do'
@@ -32,6 +48,30 @@ class Task(AbsFetchTask):
         if t == 'vc':
             return self._new_vc()
         pass
+
+    def _prepare(self, data=None):
+        super()._prepare(data)
+
+        self.dsc = DriverRequestsCoordinator(s=self.s, create_driver=self._create_driver)
+
+    def _create_driver(self):
+        driver = new_driver(user_agent=USER_AGENT)
+
+        # 不加载验证码
+        script = """
+        var page = this;
+        page.onResourceRequested = function(requestData, networkRequest) {
+            var match = requestData.url.match(/PicCheckCode1/g);
+            if (match != null) {
+                //console.log('Request (#' + requestData.id + '): ' + JSON.stringify(requestData));
+                //networkRequest.cancel(); // or .abort()
+                networkRequest.abort();
+            }
+        };
+        """
+        driver.execute('executePhantomScript', {'script': script, 'args': []})
+
+        return driver
 
     def _setup_task_units(self):
         """设置任务执行单元"""
@@ -79,26 +119,13 @@ class Task(AbsFetchTask):
                 username=params['用户名']
                 password =params['密码']
                 vc = params['vc']
-                resp = self.s.post(LOGIN_URL, data=dict(
-                    r=random.random(),
-                    LOGINID=username,
-                    PASSWORD=password,
-                    IMAGCHECK=vc,
-                    OPERTYPE2=3,
-                    ISBIND='false',
-                    now=time.strftime('%a %b %d %Y %H:%M:%S', time.localtime()),
-                    callback=''
-                ))
-                soup = BeautifulSoup(resp.content, 'html.parser')
-                jsonread = json.loads(soup.text)
-                errormsg = jsonread['message']
-                if errormsg:
-                    raise InvalidParamsError(errormsg)
-                self.result_key = params.get('username')
+                self._do_login(username, password, vc)
+                # 登录成功
+                self.result_key = username
 
                 # 保存到meta
-                self.result_meta['用户名'] = params.get('用户名')
-                self.result_meta['密码'] = params.get('密码')
+                self.result_meta['用户名'] = username
+                self.result_meta['密码'] = password
 
                 self.result_identity['task_name']='深圳'
 
@@ -111,7 +138,42 @@ class Task(AbsFetchTask):
             dict(key='密码', name='密码', cls='input:password', value=params.get('密码', '')),
             dict(key='vc', name='验证码', cls='data:image', query={'t': 'vc'}, value=params.get('vc', '')),
         ], err_msg)
+    def _do_login(self, username, password, vc):
+        """使用web driver模拟登录过程"""
+        with self.dsc.get_driver_ctx() as driver:
+            # 打开登录页
+            driver.get(LOGIN_PAGE_URL)
+            # 等待lk请求
+            WebDriverWait(driver, 10).until(value_is_number((By.XPATH, '//*[@id="lk"]')))
 
+            # 选择身份证号方式登录
+            driver.find_element_by_xpath('/html/body/table[2]/tbody/tr[3]/td/table/tbody/tr/td/div/form/div[1]/ul/li[3]/a').click()
+
+            username_input = driver.find_element_by_xpath('//*[@id="bh1"]')
+            password_input = driver.find_element_by_xpath('//*[@id="mm1"]')
+            vc_input = driver.find_element_by_xpath('//*[@id="login_tab_2"]/div/div[3]/input')
+            submit_btn = driver.find_element_by_xpath('//*[@id="login_tab_2"]/div/div[4]/input[1]')
+
+            # 用户名
+            username_input.clear()
+            username_input.send_keys(username)
+
+            # 密码
+            password_input.clear()
+            password_input.send_keys(password)
+            vc_input.clear()
+            vc_input.send_keys(vc)
+            # 提交
+            submit_btn.click()
+
+            if not driver.current_url == 'http://www.bjgjj.gov.cn/wsyw/wscx/gjjcx-choice.jsp':
+                raise InvalidParamsError('登录失败，请检查输入')
+
+            # 登录成功
+
+            # 保存登录后的页面内容供抓取单元解析使用
+            self.g.login_page_html = driver.find_element_by_tag_name('html').get_attribute('innerHTML')
+            self.g.current_url = driver.current_url
     def _unit_fetch_userinfo(self):
         """用户信息"""
         try:
@@ -236,5 +298,5 @@ class Task(AbsFetchTask):
 if __name__ == '__main__':
     from services.client import TaskTestClient
     meta = {'用户名': 'keguangping', '密码': 'Kegp850907'}
-    client = TaskTestClient(Task(SessionData(), prepare_data=dict(meta=meta), is_start=False))
+    client = TaskTestClient(Task(prepare_data=dict(meta=meta)))
     client.run()
