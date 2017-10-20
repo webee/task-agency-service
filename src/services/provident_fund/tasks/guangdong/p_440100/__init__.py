fb0c9f138d8ad913aa35429a19808d738137c971
boyuanyuan <byy@qinqinxiaobao.com>
2017-10-20 15:27:52 +0800

    update

---
+++ src/services/provident_fund/tasks/guangdong/p_440100/__init__.py
@@ -0,0 +1,96 @@
+from services.service import SessionData
+from services.service import AskForParamsError, PreconditionNotSatisfiedError, TaskNotAvailableError
+from services.errors import InvalidParamsError, TaskNotImplementedError
+from services.commons import AbsFetchTask
+
+
+class Task(AbsFetchTask):
+    task_info = dict(
+        city_name="广州",
+        help="""首次申请密码或遗忘网上登陆密码，本人须携带有效身份证件至就近街道社区事务受理中心或就近社保分中心自助机具上申请办理"""
+    )
+
+    def _prepare(self):
+        """恢复状态，初始化结果"""
+        super()._prepare()
+        # state
+        # state: dict = self.state
+        # TODO: restore from state
+
+        # result
+        # result: dict = self.result
+        # TODO: restore from result
+
+    def _update_session_data(self):
+        """保存任务状态"""
+        super()._update_session_data()
+        # state
+        # state: dict = self.state
+        # TODO: update state
+
+        # result
+        # result: dict = self.result
+        # TODO: update temp result
+
+    def _get_common_headers(self):
+        return {}
+
+    def _query(self, params: dict):
+        """任务状态查询"""
+        pass
+
+    def _setup_task_units(self):
+        """设置任务执行单元"""
+        self._add_unit(self._unit_login)
+        self._add_unit(self._unit_fetch, self._unit_login)
+
+    def _check_login_params(self, params):
+        assert params is not None, '缺少参数'
+        assert '账号' in params, '缺少账号'
+        assert '密码' in params, '缺少密码'
+        # other check
+        账号 = params['账号']
+        密码 = params['密码']
+        if len(密码) < 4:
+            raise InvalidParamsError('账号或密码错误')
+        if 账号.isdigit():
+            if len(账号) < 15:
+                raise InvalidParamsError('身份证错误')
+            return
+        if '@' in 账号:
+            if not 账号.endswith('@hz.cn'):
+                raise InvalidParamsError('市民邮箱错误')
+            return
+        raise InvalidParamsError('账号或密码错误')
+
+    def _unit_login(self, params: dict):
+        err_msg = None
+        if params:
+            try:
+                self._check_login_params(params)
+                self.result_key = params.get('账号')
+                # 保存到meta
+                self.result_meta['账号'] = params.get('账号')
+                self.result_meta['密码'] = params.get('密码')
+
+                raise TaskNotImplementedError('查询服务维护中')
+            except (AssertionError, InvalidParamsError) as e:
+                err_msg = str(e)
+
+        raise AskForParamsError([
+            dict(key='账号', name='账号', cls='input', placeholder='身份证号或者市民邮箱(@hz.cn)', value=params.get('账号', '')),
+            dict(key='密码', name='密码', cls='input:password', value=params.get('密码', '')),
+        ], err_msg)
+
+    def _unit_fetch(self):
+        try:
+            # TODO: 执行任务，如果没有登录，则raise PermissionError
+            return
+        except PermissionError as e:
+            raise PreconditionNotSatisfiedError(e)
+
+
+if __name__ == '__main__':
+    from services.client import TaskTestClient
+    client = TaskTestClient(Task(SessionData()))
+    client.run()
