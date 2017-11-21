cf1b0c6a1c7fa6f81b81707cf38022d4c4669cf1
boyuanyuan <byy@qinqinxiaobao.com>
2017-11-21 17:58:32 +0800

    update

--- src/services/provident_fund/tasks/heilongjiang/p_230100/__init__.py
+++ src/services/provident_fund/tasks/heilongjiang/p_230100/__init__.py
@@ -147,7 +147,7 @@ class Task(AbsFetchTask):
                 cell = [i.text for i in row.find_all('td')]
                 if len(cell)==4:
                     data['baseInfo'][cell[0].replace('\n','').replace('账户余额','当前余额').replace('本年支取总额','当年提取金额').replace('本年缴存总额','当年缴存金额').replace('月汇缴金额','月应缴额').replace('个人缴存基数','缴存基数').replace('    ','').replace('身份证号','证件号')] = re.sub('[\n              \t  \n\r]','',cell[1].replace('\xa0',''))
-                    data['baseInfo'][cell[2].replace('\n','').replace('状 态','帐户状态').replace('最后汇缴年月','最后业务日期').replace('\r                \xa0','').replace('    ','')] = re.sub('[\n              \t  \n\r]','',cell[3].replace('\xa0','').replace('-',''))
+                    data['baseInfo'][cell[2].replace('\n','').replace(' ','').replace('状态','帐户状态').replace('最后汇缴年月','最后业务日期').replace('\r                \xa0','').replace('    ','')] = re.sub('[\n              \t  \n\r]','',cell[3].replace('\xa0','').replace('-',''))
 
             self.result_identity['target_name'] = data['baseInfo']['姓名']
             self.result_identity['status'] = ''
@@ -174,7 +174,8 @@ class Task(AbsFetchTask):
 
 if __name__ == '__main__':
     from services.client import TaskTestClient
-    meta = {'身份证号': '230223197310180837','个人账号':'801016453429', '密码': '111111'}
+    meta = {'身份证号': '230121199412264821','个人账号':'801016473025', '密码': '111111'}
 
     client = TaskTestClient(Task(prepare_data = dict(meta=meta)))
     client.run()
+#{'身份证号': '230223197310180837','个人账号':'801016453429', '密码': '111111'}
--- src/services/provident_fund/tasks/sichuan/p_510100/__init__.py
+++ src/services/provident_fund/tasks/sichuan/p_510100/__init__.py
@@ -29,34 +29,125 @@ class Task(AbsFetchTask):
         if params["other"] == "1":
             assert 'bh3' in params, '缺少个人账号'
             assert 'mm3' in params, '缺少密码'
+            个人账号=params['bh3']
+            密码 = params['mm3']
+            if len(密码) < 4:
+                raise InvalidParamsError('个人账号或密码错误')
+            if len(个人账号) < 5:
+                raise InvalidParamsError('个人账号错误')
         elif params["other"] == "2":
             assert 'bh4' in params, '缺少身份证号码'
             assert 'mm4' in params, '缺少密码'
             r = r'(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)'
-            assert re.findall(r, params['bh1']), '请输入有效的身份证编号'
+            assert re.findall(r, params['bh4']), '请输入有效的身份证编号'
+            身份证号码 = params['bh4']
+            密码 = params['mm4']
+            if len(密码) < 4:
+                raise InvalidParamsError('身份证号码或密码错误')
+            if 身份证号码.isdigit():
+                if len(身份证号码) < 15:
+                    raise InvalidParamsError('身份证号码错误')
+                return
+            raise InvalidParamsError('身份证号码或密码错误')
         elif params["other"] == "3":
-            assert 'bh5' in params, '缺少缺少联名卡号'
+            assert 'bh5' in params, '缺少联名卡号'
             assert 'mm5' in params, '缺少密码'
-        # other check
-        账号 = params['账号']
-        密码 = params['密码']
-        if len(密码) < 4:
-            raise InvalidParamsError('账号或密码错误')
-        if 账号.isdigit():
-            if len(账号) < 5:
-                raise InvalidParamsError('身份证错误')
-            return
-        raise InvalidParamsError('账号或密码错误')
+            联名卡号 = params['bh5']
+            密码 = params['mm5']
+            if len(密码) < 4:
+                raise InvalidParamsError('个人账号或密码错误')
+            if len(联名卡号) < 5:
+                raise InvalidParamsError('联名卡号错误')
+            # other check
+
+    def _params_handler(self, params: dict):
+        if not (self.is_start and not params):
+            meta = self.prepared_meta
+            if 'bh3' not in params:
+                params['bh3'] = meta.get('账号')
+            if 'mm3' not in params:
+                params['mm3'] = meta.get('密码')
+            if 'bh4' not in params:
+                params['bh4'] = meta.get('账号')
+            if 'mm4' not in params:
+                params['mm4'] = meta.get('密码')
+            if 'bh5' not in params:
+                params['bh5'] = meta.get('账号')
+            if 'mm5' not in params:
+                params['mm5'] = meta.get('密码')
+            if 'other' not in params:
+                params['other'] = meta.get('类型Code')
+        return params
+
+    def _param_requirements_handler(self, param_requirements, details):
+        meta = self.prepared_meta
+        res = []
+        for pr in param_requirements:
+            # TODO: 进一步检查details
+            if meta['类型Code'] == '1':
+                if pr['key'] == 'bh3' and '账号' in meta:
+                    continue
+                elif pr['key'] == 'mm3' and '密码' in meta:
+                    continue
+                res.append(pr)
+            # 身份证
+            elif meta['类型Code'] == '2':
+                if pr['key'] == 'bh4' and '账号' in meta:
+                    continue
+                elif pr['key'] == 'mm4' and '密码' in meta:
+                    continue
+                res.append(pr)
+            # 联名卡
+            elif meta['类型Code'] == '3':
+                if pr['key'] == 'bh5' and '账号' in meta:
+                    continue
+                elif pr['key'] == 'mm5' and '密码' in meta:
+                    continue
+                res.append(pr)
+            else:
+                res.append(pr)
+        return res
 
     def _unit_login(self, params: dict):
         err_msg = None
         if params:
             try:
                 self._check_login_params(params)
-                self.result_key = params.get('账号')
+                if params["other"] == "1":
+                    code = "3"
+                elif params["other"] == "2":
+                    code = "4"
+                else:
+                    code = "5"
+                username = params['bh' + code]
+                password = params['mm' + code]
+                vc = params['vc']
+                # data = dict(
+                #     cxydmc='当前年度',
+                #     zgzh='',
+                #     zgxm1=account_num,
+                #     sfzh=id_num,
+                #     password=password,
+                #     yzm=vc
+                # )
+                # resp = self.s.post(LOGIN_URL, data=parse.urlencode(data, encoding='gbk'),
+                #                    headers={'Content-Type': 'application/x-www-form-urlencoded'})
+                #
+                # soup = BeautifulSoup(resp.content, 'html.parser')
+
+                # return_message = soup.find('head')  # soup.find('input', {'name': 'zgzh'})["value"]
+                # if len(return_message.text) > 3:
+                #     return_message = return_message.text.split(';')[0].split('"')[1]
+                #     raise InvalidParamsError(return_message)
+                # else:
+                #     print("登录成功！")
+
+
+                self.result_key = username
+
                 # 保存到meta
-                self.result_meta['账号'] = params.get('账号')
-                self.result_meta['密码'] = params.get('密码')
+                self.result_meta['账号'] = username
+                self.result_meta['密码'] = password
 
                 raise TaskNotImplementedError('查询服务维护中')
             except (AssertionError, InvalidParamsError) as e:
@@ -66,13 +157,13 @@ class Task(AbsFetchTask):
             dict(key='other',
                  name='[{"tabName":"个人账号","tabCode":"1","isEnable":"1"},{"tabName":"身份证号","tabCode":"2","isEnable":"1"},{"tabName":"联名卡号","tabCode":"3","isEnable":"1"}]',
                  cls='tab', value=params.get('类型Code', '')),
-            dict(key='bh3', name='个人账号', cls='input', placeholder='个人账号', value=params.get('个人账号', '')),
-            dict(key='mm3', name='密码', cls='input:password', value=params.get('密码', '')),
-            dict(key='bh4', name='身份证号', cls='input', tabCode="3", value=params.get('账号', '')),
-            dict(key='mm4', name='密码', cls='input:password', tabCode="3", value=params.get('密码', '')),
-            dict(key='bh5', name='联名卡号', cls='input', tabCode="1", value=params.get('账号', '')),
-            dict(key='mm5', name='密码', cls='input:password', tabCode="1", value=params.get('密码', '')),
-            dict(key='vc', name='验证码', cls='data:image', query={'t': 'vc'}, tabCode="[3,1]", value=''),
+            dict(key='bh3', name='个人账号', cls='input', tabCode="1", placeholder='个人账号', value=params.get('账号', '')),
+            dict(key='mm3', name='密码', cls='input:password', tabCode="1", value=params.get('密码', '')),
+            dict(key='bh4', name='身份证号', cls='input', tabCode="2", value=params.get('账号', '')),
+            dict(key='mm4', name='密码', cls='input:password', tabCode="2", value=params.get('密码', '')),
+            dict(key='bh5', name='联名卡号', cls='input', tabCode="3", value=params.get('账号', '')),
+            dict(key='mm5', name='密码', cls='input:password', tabCode="3", value=params.get('密码', '')),
+            dict(key='vc', name='验证码', cls='data:image', query={'t': 'vc'}, tabCode="[1,2,3]", value=''),
         ], err_msg)
 
     def _unit_fetch(self):
