447708bf241233362af7bac6919d865ba0ecbd46
feifei.cheng <cff@qinqinxiaobao.com>
2018-01-08 17:24:11 +0800

    update

--- src/services/social_insurance/tasks/shanghai/c_310100/__init__.py
+++ src/services/social_insurance/tasks/shanghai/c_310100/__init__.py
@@ -379,7 +379,7 @@ if __name__ == '__main__':
     # client = TaskTestClient(Task(SessionData()))
     # client.run()
 
-    meta = {'用户名': '320981198307170716', '密码': '218407'}
+    meta = {'用户名': '420624198908060088', '密码': '860088'}
     client = TaskTestClient(Task(prepare_data=dict(meta=meta)))
     client.run()
 
